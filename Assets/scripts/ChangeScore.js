9ece1d4794e6ef4417288ecf01c0a7587074f486
snowson <huxueyangs@gmail.com>
2014-05-15 22:57:58 +0800

    gui

---
+++ Assets/scripts/ChangeScore.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var score_text : GameObject;
+var distance_text : GameObject;
+
+var a = new Array();
+function Start () {
+	a.push(1);
+	Debug.Log(a);
+}
+
+private var cur_score 	 : int = 0;
+private var cur_distance : int = 0;
+
+function increaseScore( delta : int ){
+	cur_score += delta;
+}
+
+function decreaseScore( delta : int  ){
+	cur_score -= delta;
+}
+
+function increaseDistance( delta : int ){
+	cur_distance += delta;
+}
+
+function Update () {
+
+	if( cur_score < 0){
+		// todo:
+	}
+	score_text.GetComponent(UILabel).text = ""+ cur_score ;
+	distance_text.GetComponent(UILabel).text = "" + cur_distance;
+	
+}
---
+++ Assets/scripts/Start_Button.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var Start_Page : GameObject;
+var Menu_Page : GameObject;
+
+function OnClick(){
+
+	NGUITools.SetActive(Menu_Page, true);
+	NGUITools.SetActive(Start_Page, false);
+	
+}
